da410641b4a46541a9b0ebdbf68a569e25b43b4f
zalamanish18 <zalamanish8595@gmail.com>
2026-04-17 13:55:59 +0530

    okay

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -22,23 +22,28 @@ const Products = () => {
     }
 
     return (
-        <div>
-            <h2>Products</h2>
+        <div className='p-5'>
+            <h2 className="text-2xl font-bold mb-4">Products</h2>
+
 
             {loading && <p>Loading...</p>}
             {error && <p style={{ color: 'red' }}>{error?.message}</p>}
 
-            <button onClick={handleLogout}>
-                Logout
-            </button>
+           <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded mb-4">
+        Logout
+    </button>
 
             {!loading && products?.length === 0 && (
                 <p>No products found</p>
             )}
 
-            {products?.map((p) => (
-                <p key={p.id}>{p.title}</p>
-            ))}
+           <div className="grid grid-cols-2 gap-4">
+        {products.map((p) => (
+            <div key={p.id} className="border p-3 rounded shadow">
+                {p.title}
+            </div>
+        ))}
+    </div>
         </div>
     )
 }
