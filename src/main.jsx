dc34dc56819343bb91cedfcff5ee367726c39ccb
zalamanish18 <zalamanish8595@gmail.com>
2026-04-17 14:09:33 +0530

    oky

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
     </Provider>
-  
+  </React.StrictMode>
 )
