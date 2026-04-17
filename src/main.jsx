e8f4b0eae01557c1f0d7c657372958af038530e7
zalamanish18 <zalamanish8595@gmail.com>
2026-04-17 14:07:12 +0530

    oky

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './app/store'
+import { store } from './app/store.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
