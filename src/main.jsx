91a0062ff1d5ec6277acc943f23688aa828e63c9
zalamanish18 <zalamanish8595@gmail.com>
2026-04-17 14:16:10 +0530

    18

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './app/store.jsx'
+import { store } from './app/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
